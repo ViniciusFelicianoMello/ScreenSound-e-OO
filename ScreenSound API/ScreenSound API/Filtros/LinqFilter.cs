07facf8e4d492069f859cebed1e1677548bdc0ab
Vinicius Mello <153565970+ViniciusFelicianoMello@users.noreply.github.com>
2024-04-08 12:01:55 -0300

    adicionando projeto de API

---
+++ ScreenSound API/ScreenSound API/Filtros/LinqFilter.cs
@@ -0,0 +1,45 @@
+﻿using ScreenSound_API.Modelos;
+
+namespace ScreenSound_API.Filtros;
+
+internal class LinqFilter
+{
+    public static void FiltrarTodosOsGenerosMusicais(List<Musica> musicas)
+    {
+        var TodosOsGenerosMusicais = musicas.Select(generos => generos.Genero).Distinct().ToList();
+        foreach (var genero in TodosOsGenerosMusicais)
+        {
+            Console.WriteLine($"- {genero}");
+        }
+    }
+
+    public static void FiltrarArtistaPorGeneroMusical(List<Musica> musicas, string genero) 
+    {
+        var ArtistaPorGenero = musicas.Where(musica => musica.Genero!.Contains(genero)).Select(musica => musica.Artista).Distinct().ToList();
+        Console.WriteLine($"Exibindo os artista por genero musical >>> {genero}");
+        foreach (var artista in ArtistaPorGenero)
+        {
+            Console.WriteLine($"- {artista}");
+        }
+    }
+
+    public static void FiltrarMusicasDeUmArtista(List<Musica> musicas, string nomeDoArtista)
+    {
+        var musicasDoArtista = musicas.Where(musica => musica.Artista!.Equals(nomeDoArtista)).ToList();
+        Console.WriteLine(nomeDoArtista);
+        foreach (var musica in musicasDoArtista)    
+        {
+            Console.WriteLine($"- {musica.Nome}");
+        }
+    }
+
+    internal static void FiltrarMusicasEmCSharp(List<Musica> musicas)
+    {
+        var MusicasEmCSharp = musicas.Where(musica => musica.Tonalidade.Equals("C#")).Select(musica => musica.Nome).ToList();
+        Console.WriteLine("Musicas em C#:");
+        foreach (var musica in MusicasEmCSharp)
+        {
+            Console.WriteLine($"- {musica}") ;
+        }
+    }
+}
---
+++ ScreenSound API/ScreenSound API/Program.cs
@@ -0,0 +1,49 @@
+﻿using ScreenSound_API.Modelos;
+using System.Text.Json;
+using ScreenSound_API.Filtros;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+
+        LinqFilter.FiltrarMusicasEmCSharp(musicas);
+        //LinqFilter.FiltrarMusicasDeUmArtista(musicas, "U2");   
+        //LinqFilter.FiltrarArtistaPorGeneroMusical(musicas, "pop");
+        //LinqOrder.ExibirListaDeArtistasOrdenados(musicas);
+        //LinqFilter.FiltrarTodosOsGenerosMusicais(musicas);
+        //musicas[0].ExibirDetalhesDaMusica();
+        //Console.WriteLine(musicas.Count); /*Quantas Musicas tem na API*/
+
+        var MusicasPreferidasVinicius = new MusicasPreferidas("Vinicius");
+        MusicasPreferidasVinicius.AdicionarMusicasFavoritas(musicas[0]);
+        MusicasPreferidasVinicius.AdicionarMusicasFavoritas(musicas[1]);
+        MusicasPreferidasVinicius.AdicionarMusicasFavoritas(musicas[377]);
+        MusicasPreferidasVinicius.AdicionarMusicasFavoritas(musicas[4]);
+        MusicasPreferidasVinicius.AdicionarMusicasFavoritas(musicas[6]);
+        MusicasPreferidasVinicius.AdicionarMusicasFavoritas(musicas[1467]);
+
+        MusicasPreferidasVinicius.ExibirMusicasFavoritas();
+
+        MusicasPreferidasVinicius.GerarArquivoJson();
+
+        MusicasPreferidasVinicius.GerarDocumentoTXTComAsMusicasFavoritas();
+
+        //var MusicasPreferidasGabriela = new MusicasPreferidas("Gabriela");
+        //MusicasPreferidasGabriela.AdicionarMusicasFavoritas(musicas[7]);
+        //MusicasPreferidasGabriela.AdicionarMusicasFavoritas(musicas[735]);
+        //MusicasPreferidasGabriela.AdicionarMusicasFavoritas(musicas[495]);
+        //MusicasPreferidasGabriela.AdicionarMusicasFavoritas(musicas[113]);
+        //MusicasPreferidasGabriela.AdicionarMusicasFavoritas(musicas[28]);
+        //MusicasPreferidasGabriela.AdicionarMusicasFavoritas(musicas[1258]);
+
+        //MusicasPreferidasGabriela.ExibirMusicasFavoritas();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"temos um problema: {ex.Message}");
+    }
+}
