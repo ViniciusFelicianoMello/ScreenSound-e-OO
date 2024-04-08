8330ec82d9248f9462086f09b6a7c141b8558ff8
Vinicius Mello <153565970+ViniciusFelicianoMello@users.noreply.github.com>
2024-04-08 12:02:58 -0300

    adicionando projeto orientação a objetos

---
+++ ScreenSound POO/ConsoleApp1/Menus/MenuAvaliarUmaBanda.cs
@@ -0,0 +1,34 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menus;
+
+internal class MenuAvaliarUmaBanda : Menu
+{
+    public override void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        // Digitar a banda que deseja avaliar
+        base.Executar(bandasRegistradas);
+
+        ExibirTituloDaOpcao("Avaliar banda");
+        Console.Write("Digite o nome da banda que deseja avaliar: ");
+        string nomeDaBanda = Console.ReadLine()!;
+
+        // Se a banda existir no dicionario >> atribuir uma nota, Se nao existir voltar ao menu principal
+        if (bandasRegistradas.ContainsKey(nomeDaBanda))
+        {
+            Banda banda = bandasRegistradas[nomeDaBanda];
+            Console.Write($"Qual nota a banda {nomeDaBanda} merece? : ");
+            Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
+            banda.AdicionarNota(nota);
+            Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda} com sucesso!");
+            Console.WriteLine("\nAperte uma tecla para voltar ao menu principal!");
+            Console.ReadKey();
+            Console.Clear();        }
+        else
+        {
+            Console.WriteLine($"\nA banda {nomeDaBanda} não foi registrada!");
+            Console.WriteLine("\nAperte uma tecla para voltar ao menu principal!");
+            Console.ReadKey();
+            Console.Clear();            }
+    }
+}
---
+++ ScreenSound POO/ConsoleApp1/Modelos/Genero.cs
@@ -0,0 +1,11 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Genero
+{
+    public Genero(string nome)
+    {
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+}
